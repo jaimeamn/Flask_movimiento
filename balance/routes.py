1568d6eeb2ba329bdbae52d3d9536799901054e1
Jaime Martinez <java9017@hotmail>
2022-03-13 18:08:34 -0500

    mi primer formulario

--- balance/routes.py
+++ balance/routes.py
@@ -1,37 +1,40 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from balance import app
 
 @app.route("/")
 def inicio():
-    lista_movimiento =[
-        {
-            "fecha": "2022-01-06",
-            "hora": "13:23:34",
-            "concepto": "reyes",
-            "es_ingreso": False,
-            "cantidad": 100
-        },
-        {
-            "fecha": "2022-01-06",
-            "hora": "13:23:34",
-            "concepto": "jajajaj",
-            "es_ingreso": True,
-            "cantidad": 666
-        },
+    lista_movimientos = []
+    fichero_mv = open("data/movimientos.csv", "r")
 
-        {
-            "fecha": "2022-01-06",
-            "hora": "13:23:34",
-            "concepto": "jajajaj",
-            "es_ingreso": True,
-            "cantidad": 666
-        },
-        {
-            "fecha": "2022-01-06",
-            "hora": "13:23:34",
-            "concepto": "jkakaka",
-            "es_ingreso": False,
-            "cantidad": 180
-        }
-    ]
-    return render_template("lista_movimientos.html", movimiento = lista_movimiento)
+    #----------------------------------------------------
+    cab = fichero_mv.readline()
+    linea = fichero_mv.readline()
+    while linea != "":
+        campos = linea.split(",")
+        lista_movimientos.append(
+            {
+                "fecha": campos[0],
+                "hora": campos[1],
+                "concepto": campos[2],
+                "es_ingreso": True if campos[3] == '1' else False,
+                "cantidad": float(campos[4])
+            }
+        )
+
+        linea = fichero_mv.readline()
+
+
+    linea = fichero_mv.readline()
+
+    fichero_mv.close()
+    return render_template("lista_movimientos.html", 
+                            movimientos = lista_movimientos)
+
+@app.route("/alta", methods=["GET", "POST"])
+def alta():
+    if request.method == 'GET':
+        return render_template("nuevo_movimiento.html")
+    else:
+        # recuperar los campos del request.form
+        # grabar el nuevo registro en movimientos.csv
+        return redirect(url_for("inicio"))
